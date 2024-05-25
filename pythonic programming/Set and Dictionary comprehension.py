45bf5840e1faa15ea3ebed3369947b57a93bad8b
chydeepak7 <deepakchy793@gmail.com>
2024-05-25 10:34:00 +0545

    set and dictionary comprehension

---
+++ pythonic programming/Set and Dictionary comprehension.py
@@ -0,0 +1,10 @@
+print({x for x in range(1, 10)}) # Set Comprehension
+# strip() removes spaces in front or after the end of the string
+# split() splits the sentence by spaces
+test = '          deepak is done           '
+print(test.strip())
+print(test.split())
+test = test.strip()
+print(test)
+dict_compre = {x+1:y for x,y in enumerate(test.split())}
+print(dict_compre)
