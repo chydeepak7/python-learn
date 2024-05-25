b13d59f6bc03a78c28f50e685622987e591109aa
chydeepak7 <deepakchy793@gmail.com>
2024-05-26 00:23:20 +0545

    finding gcd

--- python basics/modules and functions/finding gcd.py
+++ python basics/modules and functions/finding gcd.py
@@ -6,7 +6,10 @@ def findGCD(a, b):
   for i in range(1,x+1):
     if a%i==0 and 0 == b%i:
       y = i
-
-
   return y# return the gcd of numbers
 print(findGCD(98,56))
+
+# OR use this method
+a = 98
+b=56
+print(math.gcd(a,b))
