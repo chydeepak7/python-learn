3ffa58c2469fee88ee7be9f36879c68a41539bd5
chydeepak7 <deepakchy793@gmail.com>
2024-05-26 13:38:33 +0545

    dictionaries

---
+++ python basics/dictionaries/dictionary basics.py
@@ -0,0 +1,12 @@
+test = {
+    'name': 'Deepak',
+    'age': 22,
+    1:'id',
+    4:2
+}
+print(test)
+print(test['name'])
+print(test[4])
+print(test.items())
+print(test.keys())
+print(test.values())
