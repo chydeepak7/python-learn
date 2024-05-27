6a66e63267b7628e3baff2b6d14d3b71117b0b35
chydeepak7 <deepakchy793@gmail.com>
2024-05-27 09:45:31 +0545

    learning file handling

--- python basics/file handling/read, write and append.py
+++ python basics/file handling/read, write and append.py
@@ -1,12 +1,15 @@
-f = open('example.txt', 'r')
-print(f.read())
+f = open('example.txt', 'a')
+print(f.write('da'))
 f.close()
-with open('example.txt',"w") as f:
-    f.write("testing")
-with open('example.txt','r') as f:
-    print(f.read())
+with open('example.txt',"a") as f:
+    f.write("testing\n")
+# with open('example.txt','r') as f:
+#     print(f.read())
 with open("example.txt",'a') as f:
     f.write('hahaha')
-f = open("example.txt", 'r')
-print(f.read())
-f.close()
+# f = open("example.txt", 'r')
+# print(f.read())
+# f.close()
+with open('example.txt', 'r') as f:
+    for i in f:
+        print(i.strip())
